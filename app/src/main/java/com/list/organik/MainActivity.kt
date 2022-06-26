9ca8ef5e60b613cfa4683ffc2fa1cefdc84a5dfc
Karina Fukuda <karina.fukuda@mercadolivre.com>
2022-06-26 01:47:30 -0300

    added FAB and logic of form

--- app/src/main/java/com/list/organik/MainActivity.kt
+++ app/src/main/java/com/list/organik/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.list.organik
 
-import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.list.organik.data.ProductData.getProducts
+import com.list.organik.view.FormProductActivity
 import com.list.organik.viewmodel.ListProductsAdapter
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
@@ -16,6 +19,12 @@ class MainActivity : Activity() {
             recyclerView.adapter = ListProductsAdapter(
                 context = this, products = getProducts()
             )
+            // acao para alterar a tela a partir do fab
+            val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+            fab.setOnClickListener {
+               val intent = Intent(this, FormProductActivity::class.java)
+                startActivity(intent)
+            }
             //Uma das formas de mostrar os dados na tela
             //recyclerView.layoutManager = LinearLayoutManager(this)
         }
--- app/src/main/java/com/list/organik/view/FormProductActivity.kt
+++ app/src/main/java/com/list/organik/view/FormProductActivity.kt
@@ -1,8 +1,48 @@
 package com.list.organik.view
 
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.list.organik.R
+import com.list.organik.model.Product
+import java.math.BigDecimal
 
 class FormProductActivity :
-    AppCompatActivity(R.layout.activity_form_product){
+    AppCompatActivity(R.layout.activity_form_product) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val btnSave = findViewById<Button>(R.id.btn_save)
+        btnSave.setOnClickListener {
+            //valor do titulo no forms
+            val fieldFormTitle = findViewById<EditText>(R.id.title_form)
+            val titleGenerated = fieldFormTitle.text.toString()
+
+            //valor da descricao no forms
+            val fieldFormProduct = findViewById<EditText>(R.id.description_form)
+            val productGenerated = fieldFormProduct.text.toString()
+
+            //valor do preco total no forms
+            val fieldFormPriceText = findViewById<EditText>(R.id.price_form)
+            val textGenerated = fieldFormPriceText.text.toString()
+
+            //valida se vem vazio/em branco para evitar erro ao clicar sem valor
+            val newPrice = if (textGenerated.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(textGenerated)
+            }
+            //cria novo produto
+            val newProduct = Product(
+                title = titleGenerated,
+                products = productGenerated,
+                price = newPrice
+            )
+            Log.i("FormProduct", "onCreate: $newProduct")
+        }
+    }
+
 }
