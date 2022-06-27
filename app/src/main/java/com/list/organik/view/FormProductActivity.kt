d694d7dc3f81f330810d5c0b8eafbf7dd1c5bc67
Karina Fukuda <karina.fukuda@mercadolivre.com>
2022-06-26 21:17:50 -0300

    Refactor FormProductActivity

--- app/src/main/java/com/list/organik/view/FormProductActivity.kt
+++ app/src/main/java/com/list/organik/view/FormProductActivity.kt
@@ -17,37 +17,39 @@ class FormProductActivity :
         super.onCreate(savedInstanceState)
 
         val btnSave = findViewById<Button>(R.id.btn_save)
+        val dao = ProductDao()
+
         btnSave.setOnClickListener {
-            //valor do titulo no forms
-            val fieldFormTitle = findViewById<EditText>(R.id.title_form)
-            val titleGenerated = fieldFormTitle.text.toString()
-
-            //valor da descricao no forms
-            val fieldFormProduct = findViewById<EditText>(R.id.description_form)
-            val productGenerated = fieldFormProduct.text.toString()
-
-            //valor do preco total no forms
-            val fieldFormPriceText = findViewById<EditText>(R.id.price_form)
-            val textGenerated = fieldFormPriceText.text.toString()
-
-            //valida se vem vazio/em branco para evitar erro ao clicar sem valor
-            val newPrice = if (textGenerated.isBlank()) {
-                BigDecimal.ZERO
-            } else {
-                BigDecimal(textGenerated)
-            }
-            //cria novo produto
-            val newProduct = Product(
-                title = titleGenerated,
-                products = productGenerated,
-                price = newPrice
-            )
-            Log.i("FormProduct", "onCreate: $newProduct")
-            val dao = ProductDao()
+            val newProduct = createProduct()
             dao.add(newProduct)
-            Log.i("FormProduct", "onAdd: ${dao.getAllProducts()}")
             finish()
         }
     }
 
+    private fun createProduct(): Product {
+        //valor do titulo no forms
+        val fieldFormTitle = findViewById<EditText>(R.id.title_form)
+        val titleGenerated = fieldFormTitle.text.toString()
+
+        //valor da descricao no forms
+        val fieldFormProduct = findViewById<EditText>(R.id.description_form)
+        val productGenerated = fieldFormProduct.text.toString()
+
+        //valor do preco total no forms
+        val fieldFormPriceText = findViewById<EditText>(R.id.price_form)
+        val textGenerated = fieldFormPriceText.text.toString()
+
+        //valida se vem vazio/em branco para evitar erro ao clicar sem valor
+        val newPrice = if (textGenerated.isBlank()) {
+            BigDecimal.ZERO
+        } else {
+            BigDecimal(textGenerated)
+        }
+        //cria novo produto
+        return Product(
+            title = titleGenerated,
+            products = productGenerated,
+            price = newPrice
+        )
+    }
 }
